7058717fa539fd2f716aede049e4dad475654750
SANGWOO PARK <cyberprophet@hanmail.net>
2022-08-15 11:53:39 +0900

    초기 커밋.

---
+++ Maui/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using Maui.Data;
+namespace Maui
+{
+
+
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("CascadiaCode.ttf", "CascadiaCode");
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+
+            builder.Services.AddSingleton<WeatherForecastService>();
+
+            return builder.Build();
+        }
+    }
+}
